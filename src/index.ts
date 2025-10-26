42fa4db0daa7dbf9dfa2cfc632d9753804217155
Lucas Moraes <lucas.moraes88@yahoo.com>
2025-10-26 00:27:48 -0300

    chore: cors

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { movementRoutes } from "./presentation/routes/Movement.routes";
 import { categoryRoutes } from "./presentation/routes/Category.routes";
 import { authRoutes } from "./presentation/routes/Auth.routes";
 import { HTTPException } from "hono/http-exception";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+app.use("*", cors());
 app.get("/", (c) => c.json({ message: "hello" }));
 app.route("/api", authRoutes);
 app.route("/api", movementRoutes);
