dd2322eae09710488f88611d436ad8f3a5469d28
Lucas Moraes <lucas.moraes88@yahoo.com>
2025-10-31 11:23:55 -0300

    chore: auth route

--- src/presentation/routes/Auth.routes.ts
+++ src/presentation/routes/Auth.routes.ts
@@ -1,12 +1,8 @@
 import { logIn, logOut } from "../auth/index.auth";
 import { Hono } from "hono";
 import { validateAuthLogIn } from "../middleware/AuthValidation.middleware";
-import { cors } from "hono/cors";
-import { CORS_OPTIONS } from "../constants";
 
 const router = new Hono().basePath("/api");
-
-router.use("/api/*", cors(CORS_OPTIONS));
 
 router.post("/auth/login", async (c, next) => {
   await validateAuthLogIn(c, next);
